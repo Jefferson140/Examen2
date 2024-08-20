7d5416730877104b3d7485227587d200a845ba1a
Ibisay <ibgutierrezal@est.utn.ac.cr>
2024-08-20 15:29:53 -0600

    Se programo la clase ListaTickets

--- Tickets/ListaTickets.java
+++ Tickets/ListaTickets.java
@@ -4,10 +4,40 @@
  */
 package Tickets;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Student
  */
 public class ListaTickets {
+    private ArrayList<String> Lista;
+
+    public ArrayList<String> getLista() {
+        return Lista;
+    }
+
+    public ListaTickets(ArrayList<String> Lista) {
+        this.Lista = Lista;
+    }
+
+    
+    public ListaTickets() {
+        Lista=new ArrayList<>();
+                
+    }
+    
+    public void agregarElemento(String Elemento){
+        Lista.add(Elemento);
+    }
+    public boolean eliminarElemento(String Elemento){
+     return Lista.remove(Elemento);
+     
+    }
+    public void mostrarElementos() {
+        for (String elemento : Lista) {
+            System.out.println(elemento);
+        }
+    }
     
 }
